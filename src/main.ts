d7a04db930421ce91831fdc17ff3e0c61cc39a6b
danila <test@test.ru>
2024-03-17 22:55:30 +0300

    removing unnecessary libraries

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,14 @@ import App from "./App.vue";
 import { createApp } from "vue";
 import { createRouter } from "./router";
 import { createAuth0 } from "@auth0/auth0-vue";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faLink, faUser, faPowerOff } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import {authConfig} from "@/app/config";
-import hljs from 'highlight.js/lib/core';
-import json from 'highlight.js/lib/languages/json';
-import hljsVuePlugin from "@highlightjs/vue-plugin";
-import "highlight.js/styles/github.css";
 
-hljs.registerLanguage('json', json);
 
 const app = createApp(App);
 
-library.add(faLink, faUser, faPowerOff);
+
 
 app
-  .use(hljsVuePlugin)
   .use(createRouter(app))
   .use(
     createAuth0({
@@ -29,5 +20,4 @@ app
       }
     })
   )
-  .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
