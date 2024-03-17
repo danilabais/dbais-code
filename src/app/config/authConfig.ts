0713f8f964de847210566cf28ecb0802dba03730
danila <test@test.ru>
2024-03-17 22:45:29 +0300

    init and change local config

---
+++ src/app/config/authConfig.ts
@@ -0,0 +1,5 @@
+
+export const authConfig = {
+    domain: import.meta.env.VITE_AUTH_DOMEN,
+    clientId: import.meta.env.VITE_CLIENT_ID
+}
---
+++ vite-env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv extends Readonly<Record<string, string>>  {
+  readonly VITE_AUTH_DOMEN: string
+  readonly VITE_CLIENT_ID: string
+}
+
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
